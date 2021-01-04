904333f58aafa6f5c8b9da1be23b05f2d34188ae
OlehBabiak <oleg_bob@ukr.net>
2021-01-04 21:48:47 +0200

    Last

--- 01_types.js
+++ 01_types.js
@@ -1,13 +1,22 @@
 // Types of data: Number, String, Null, Undefined, Boolean, symbol, Object
-console.log(typeof 0)
-console.log(typeof '0')
-console.log(typeof null)
-console.log(typeof undefined)
-console.log(typeof true)
-console.log(typeof Math)
-console.log(typeof Symbol('JS'))
-console.log(typeof function () {})
-console.log(typeof NaN)
+// node назва_файлу.js - запускає консоль
+// ctrl+shift+P вибираєм: terminal: clear - очистить консоль
 
-rturyi
-iuir7iiiii78i
+// console.log(typeof 0)
+// console.log(typeof '0')
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof true)
+// console.log(typeof Math)
+// console.log(typeof Symbol('JS'))
+// console.log(typeof function () {})
+// console.log(typeof NaN)
+// console.log(undefined * 1)
+
+
+// зведення типів
+
+let language = 'Java Script'
+if (language) {
+    console.log('The best language is:', language)
+}
