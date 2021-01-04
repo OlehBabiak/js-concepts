1fc7517a38064d609b1aec58992ad1fa03b33f1f
OlehBabiak <oleg_bob@ukr.net>
2021-01-04 21:39:48 +0200

    first commit

---
+++ 01_types.js
@@ -0,0 +1,12 @@
+// Types of data: Number, String, Null, Undefined, Boolean, symbol, Object
+console.log(typeof 0)
+console.log(typeof '0')
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof Math)
+console.log(typeof Symbol('JS'))
+console.log(typeof function () {})
+console.log(typeof NaN)
+
+
